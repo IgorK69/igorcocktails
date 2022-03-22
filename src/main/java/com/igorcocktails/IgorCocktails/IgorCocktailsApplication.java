c698d000da5c38a3e835a09e967c7b2b4f2847e7
= <=>
2022-03-22 11:39:30 +0100

    first release

---
+++ src/main/java/com/igorcocktails/IgorCocktails/IgorCocktailsApplication.java
@@ -0,0 +1,13 @@
+package com.igorcocktails.IgorCocktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgorCocktailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgorCocktailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorcocktails/IgorCocktails/IgorCocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorcocktails.IgorCocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgorCocktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
